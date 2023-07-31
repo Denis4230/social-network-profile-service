ca8028a3c02f8dbf9af28bfcc7355897f911d6d0
Denis4230 <92786441+Denis4230@users.noreply.github.com>
2023-07-31 12:35:25 +0700

    TASK-N3-1

--- src/main/resources/liquibase/sql/semenovio_SN2-init-schema.sql
+++ src/main/resources/liquibase/sql/semenovio_SN2-init-schema.sql
@@ -2,12 +2,3 @@
 --changeset semenovio:SN1-init-schema
 
 CREATE SCHEMA snprofile;
-CREATE TABLE snprofile.profile
-(
-    `id`        INT         NOT NULL,
-    `userId`    INT         NOT NULL,
-    `firstName` VARCHAR(45) NOT NULL,
-    `lastName`  VARCHAR(45) NOT NULL,
-    `birthdate` DATE NULL,
-    PRIMARY KEY (`id`)
-);
